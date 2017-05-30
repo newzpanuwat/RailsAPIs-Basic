a11bfc9e951b7c4adbf8e78c7e13ac4779f82ded
Earth <virudson@gmail.com>
2017-05-31 00:32:24 +0700

    add show action spec

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Api::V1::UsersController, type: :request, api: true do
     it { expect(response).to have_http_status(:ok) }
     it { expect(response).to match_response_schema(:users, strict: true) }
   end
+
+  describe 'GET #show' do
+    let(:user) { User.last }
+
+    context 'success' do
+      before { get api_v1_user_path(user) }
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to match_response_schema(:user, strict: true) }
+    end
+
+    context 'failure' do
+      before { get api_v1_user_path(id: '0') }
+
+      it { expect(response).to have_http_status(:not_found) }
+      it { expect(response.body).to match(/record not found/)}
+    end
+  end
 end
